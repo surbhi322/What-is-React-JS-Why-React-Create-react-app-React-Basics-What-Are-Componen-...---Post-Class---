9e47910744b4a578593bd902a1a6f2693decca85
surbhi322 <81092552+surbhi322@users.noreply.github.com>
2022-05-19 16:43:49 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,17 @@
 // write code for Welcome component here
+
+import React from 'react'
+
+ function Welcome  (props) {
+  return (
+    <>
+    <h1>Hey !{props.name}</h1>
+    <h2>Welcome to Newton School</h2>
+    </>
+  )
+}
+
+// function Welcome(){
+//   return (<h1>Hey</h1>);
+// }
+ export default Welcome;
